9b48b6c4c98e2ed49369836461d103cb242862ed
Bodya242 <48524067+Bodya242@users.noreply.github.com>
2019-03-14 13:34:04 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace kantor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Sum = new System.Windows.Forms.Label();
+            this.dMoney = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.Sum);
+            this.panel1.Controls.Add(this.dMoney);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(771, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // Sum
+            // 
+            this.Sum.AutoSize = true;
+            this.Sum.Location = new System.Drawing.Point(62, 334);
+            this.Sum.Name = "Sum";
+            this.Sum.Size = new System.Drawing.Size(38, 13);
+            this.Sum.TabIndex = 2;
+            this.Sum.Text = "гривні";
+            // 
+            // dMoney
+            // 
+            this.dMoney.Location = new System.Drawing.Point(34, 290);
+            this.dMoney.Name = "dMoney";
+            this.dMoney.Size = new System.Drawing.Size(100, 20);
+            this.dMoney.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 264);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "валюта";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 463);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox dMoney;
+        private System.Windows.Forms.Label Sum;
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace kantor
+{
+    public partial class Form1 : Form
+    {
+        List<money> valuta;
+        public Form1()
+        {
+            InitializeComponent();
+            valuta = new List<money>();
+            valuta.Add(new money() { Name = "$", prodaj = 26.4, kypivla = 26.2 });
+            valuta.Add(new money() { Name = "Є", prodaj = 32.2, kypivla = 31.9 });
+            
+            int startPoint = 20;
+            int elemencount = 0;
+            foreach (var item in valuta)
+            {
+                RadioButton m = new RadioButton();
+                m.Text = item.Name;
+                m.CheckedChanged += new EventHandler(m_CheckedChanged);
+                m.Parent = panel1;
+                m.Height = 20;
+                int tmpoffser = startPoint + elemencount * (m.Height + 10);
+                m.Location = new Point(20, tmpoffser);
+                m.Name = "m" + item.Name;
+
+
+                Label l = new Label();
+                l.Text = item.prodaj.ToString();
+                l.Location = new Point(120, tmpoffser);
+                l.Parent = panel1;
+                elemencount++;
+
+
+                RadioButton a = new RadioButton();
+                a.Text = item.Name;
+                a.CheckedChanged += new EventHandler(a_CheckedChanged);
+                a.Parent = panel1;
+                a.Height = 20;
+                int tmkoffser = startPoint + elemencount * (a.Height + 10);
+                a.Location = new Point(20, tmkoffser);
+                a.Name = "a" + item.Name;
+
+
+                Label k = new Label();
+                k.Text = item.kypivla.ToString();
+                k.Location = new Point(120, tmkoffser);
+                k.Parent = panel1;
+                elemencount++;
+            }
+        }
+            void m_CheckedChanged(object sender, EventArgs e)
+            {
+                RadioButton M = (RadioButton)sender;
+                sumdouble l = double.Parse(dMoney.Text);
+        
+                Sum.Text = (l * valuta.Where(i => i.Name == M.Text).FirstOrDefault().prodaj).ToString();
+            }
+            void a_CheckedChanged(object sender, EventArgs e)
+            {
+                RadioButton A = (RadioButton)sender;
+                double l = double.Parse(dMoney.Text);
+
+                Sum.Text = (l * valuta.Where(i => i.Name == A.Text).FirstOrDefault().kypivla).ToString();
+            }
+        public class money
+        {
+            public string Name;
+            public double prodaj;
+            public double kypivla;
+
+        }
+    }
+}
+
